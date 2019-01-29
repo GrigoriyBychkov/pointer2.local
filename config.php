0637cb549045479ccd44e6875a50f29c6e3ce461
GrigoriyBychkov <imansiberia@icloud.com>
2019-01-29 16:14:00 +0600

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Config {
-    public static $host = '172.104.242.116:3306';
-    public static $dbname = 'agromiks';
-    public static $username = 'lara';
-    public static $password = 'KFh48^3*kd';
-}
